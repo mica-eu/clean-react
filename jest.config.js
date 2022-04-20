92ad0512d6ad3bdf3049dcdfdf9e6b80bc8f8933
Micael Souza <5789811+micaelsouza@users.noreply.github.com>
2022-04-20 17:45:29 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
-  testEvironment: 'node',
+  testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest',
-  },
-};
+    '.+\\.ts$': 'ts-jest'
+  }
+}
